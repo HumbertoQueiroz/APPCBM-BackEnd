2f9d2ce5120fb114139798a084c87484ac0dc4ca
Dev Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2025-04-03 11:13:53 -0400

    Campos opcionais de endereço

--- src/http/routs/createUser.ts
+++ src/http/routs/createUser.ts
@@ -1,17 +1,25 @@
-import type { FastifyInstance } from 'fastify'
-import { prisma } from '../../libe/prisma'
-import bcrypt from 'bcrypt'
-import z from 'zod'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import type { FastifyInstance } from "fastify";
+import { prisma } from "../../libe/prisma";
+import bcrypt from "bcrypt";
+import z from "zod";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
 
 export async function createUser(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
-    '/user',
+    "/user",
     {
       schema: {
         body: z.object({
           userName: z.string(),
           cpf: z.string(),
+          addressFull: z.string().optional(),
+          addressLog: z.string().optional(),
+          addressNum: z.string().optional(),
+          addressBai: z.string().optional(),
+          addressCit: z.string().optional(),
+          addressEst: z.string().optional(),
+          addressCEP: z.string().optional(),
+          addressComp: z.string().optional(),
           telephone: z.string(),
           email: z.string().email(),
           password: z.string(),
@@ -19,23 +27,67 @@ export async function createUser(app: FastifyInstance) {
       },
     },
     async (request, response) => {
-      const { userName, cpf, telephone, email, password } = request.body
+      //desestruturação da requisição recebida, para pegar os campos
+      const {
+        userName,
+        cpf,
+        addressFull,
+        addressLog,
+        addressNum,
+        addressBai,
+        addressCit,
+        addressEst,
+        addressCEP,
+        addressComp,
+        telephone,
+        email,
+        password,
+      } = request.body;
+
+      //_______________________//// Validações \\\\___________________________//
 
       //Incluir validações aqui
 
-      //
+      // Validação para verificar se já existe um usuário com o CPF informado
+      const existingUserWithCpf = await prisma.user.findUnique({
+        where: { cpf: cpf },
+      });
 
-      const hashPassword = await bcrypt.hash(password, 6)
+      if (existingUserWithCpf) {
+        return response.status(400).send({ error: "CPF já cadastrado" });
+      }
+
+      // Validação para verificar se já existe um usuário com o Email informado
+      const existingUserWithEmail = await prisma.user.findUnique({
+        where: { email: email },
+      });
+
+      if (existingUserWithEmail) {
+        return response.status(400).send({ error: "Usuário já existe" });
+      }
+
+      //_______________________// Criando no Banco de Dados\\_________________//
+      //Caso passe nas validações vai criar usuário no banco de dados
+
+      //Cria hash da senha para salvar no banco de dados
+      const hashPassword = await bcrypt.hash(password, 6);
+
+      //Dados
+      let data = {
+        userName: userName,
+        cpf: cpf,
+        telephone: telephone,
+        email: email,
+        password: hashPassword,
+      };
+      if (addressFull) {
+        Object.assign(data, {addressFull:addressFull})
+      }
+
       await prisma.user.create({
-        data: {
-          userName: userName,
-          cpf: cpf,
-          telephone,
-          email,
-          password: hashPassword,
-        },
-      })
-      return response.status(201).send({"status":"Sucesse Created"})
+        data,
+      });
+      return response.status(201).send({ status: "Success Created" });
     }
-  )
+  );
 }
