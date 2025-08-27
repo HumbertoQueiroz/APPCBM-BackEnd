af23066a07aef9bb74a66e12e77cd58815d94035
Dev Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2025-08-27 11:32:02 -0400

    add

--- redme.md
+++ redme.md
@@ -1,3 +1,28 @@
 # Backend APPCBM
 
 Será utilizado `node.js`com `typescript` e framework `fastify` [documentação](https://fastify.dev/docs/latest/Guides/Getting-Started/).
+
+## Prisma e Postgresql
+
+### Criar arquivo de migração
+
+Quando realizar uma alteração no  no arquivo `schema.prisma`  executar `npx prisma migrate dev` pois ele verifica o arquivo e se teve alteração cria um arquivo de migração e aplica no banco de dados as alterações.
+
+### Enviar migrações ao banco
+
+Para enviar ao postgresql as migrations do banco (alterações realizadas no arquivo `schema.prisma`) executar:
+Este comando é usado para aplicar migrations em ambiente de produção. Ele apenas aplica as migrations que ainda não foram executadas e não cria novas.
+
+Para desenvolvimento:
+`npx prisma migrate deploy`
+
+### Mostrar status atual das migrações no banco de dados
+
+Este comando mostra o estado atual das suas migrations, indicando quais já foram aplicadas no banco de dados.
+
+`npx prisma migrate status`
+
+### Resetar o banco de dados
+
+Apaga o banco de dados e recriar aplicando as migrações
+`npx prisma migrate reset`
--- src/http/routs/occurrence.ts
+++ src/http/routs/occurrence.ts
@@ -9,23 +9,21 @@ export async function occurrence (app: FastifyInstance) {
     {
       schema: {
         body: z.object({
-          user: z.string().trim(),
-          /*
-          natOco: z.string().trim(),
-          geoLat: z.number(),
-          geoLong: z.number(),
-          addressNum: z.string().trim(),
-          addressLog: z.string().trim(),
-          addressBairro: z.string().trim(),
-          addressCity: z.string().trim(),
-          addressState: z.string().trim(),
-          addressCEP: z.string().trim(),
-          addressComp: z.string().trim(),
-          description: z.string().trim(),
+          user: z.string().email().trim().max(254, {message:"E-mail deve ter máximo de 254 caracteres"}).min(7,{message:"E-mail deve ter mínimo de 7 caracteres"}),
+          natOco: z.string().trim().min(5,{message:"Natureza da Ocorrência deve ter no mínimo 5 caracteres"}).max(50,{message:"Natureza da Ocorrência deve ter no máximo 50 caracteres"}) ,
+          geoLat: z.number().optional(),
+          geoLong: z.number().optional(),
+          addressNum: z.string().trim().max(254, {message:"Numero do endereço deve ter máximo de 254 caracteres"}).optional(),
+          addressLog: z.string().trim().max(500, {message:"Logradouro/Rua/Avenida do endereço deve ter máximo de 500 caracteres"}).optional(),
+          addressBairro: z.string().trim().max(254, {message:"Bairro do endereço deve ter máximo de 254 caracteres"}).optional(),
+          addressCity: z.string().trim().max(254, {message:"Cidade do endereço deve ter máximo de 254 caracteres"}).optional(),
+          addressState: z.string().trim().max(254, {message:"Estado do endereço deve ter máximo de 254 caracteres"}).optional(),
+          addressCEP: z.string().trim().max(9, {message:"CEP do endereço deve ter máximo de 9 caracteres"}).optional(),
+          addressComp: z.string().trim().max(500, {message:"Complemento do endereço deve ter máximo de 500 caracteres"}).optional(),
+          description: z.string().trim().max(1000, {message:"Descrição da ocorrência deve ter máximo de 1000 caracteres"}).min(7,{message:"Descrição da ocorrência deve ter mínimo de 7 caracteres"}),
           hasVictim: z.boolean(),
-          victimsQuantity: z.number(),
-          conditionVictim: z.string().trim(),
-          */
+          victimsQuantity: z.number().optional(),
+          conditionVictim: z.string().trim().max(254, {message:"Condição da(s) vítima(s) deve ter máximo de 254 caracteres"}).optional(),
         }),
       },
     },
@@ -33,7 +31,6 @@ export async function occurrence (app: FastifyInstance) {
       //desestruturação da requisição recebida, para pegar os campos
       const {
           user,
-          /*
           natOco,
           geoLat,
           geoLong,
@@ -47,8 +44,7 @@ export async function occurrence (app: FastifyInstance) {
           description,
           hasVictim,
           victimsQuantity,
-          conditionVictim,
-          */
+          conditionVictim,          
       } = request.body;
 
       const emailLocaleLowerCase = user.toLocaleLowerCase()
@@ -58,14 +54,33 @@ export async function occurrence (app: FastifyInstance) {
         where: { email: emailLocaleLowerCase },
       });
 
+      
       if (!existingUserWithEmail) {
         console.log("======= //// Erro de Validação: Usuário não existe ///// =======");
         return response.status(401).send({ message: "cod03: Não autorizado registrar ocorrência, usuário não registrado" });
       }
-
+      
       if(existingUserWithEmail){
+        const dataOccurrence = {
+          userId: existingUserWithEmail.id,
+          natOco,geoLat,
+          geoLong,
+          addressNum,
+          addressLog,
+          addressBairro,
+          addressCity,
+          addressState,
+          addressCEP,
+          addressComp,
+          description,
+          hasVictim,
+          victimsQuantity,
+          conditionVictim, 
+
+        }
         
-        console.log(existingUserWithEmail)
+        console.log("dados do user: ",existingUserWithEmail)
+        console.log("dados da ocorrência: ",dataOccurrence)
       
         return response.status(200).send({ message: "Autorizado login." });
       }
