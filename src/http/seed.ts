267fd3636cd05884edcba9341548deed596f02cf
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2025-11-04 21:40:40 -0400

    add

--- src/http/seed.ts
+++ src/http/seed.ts
@@ -1,35 +1,34 @@
-import { prisma } from '../libe/prisma'
+import { prisma } from "../libe/prisma";
 import bcrypt from "bcrypt";
 
 async function seed() {
-
-  const hashPassword = await bcrypt.hash('987456321', 6);
-    await prisma.user.create({
-      data: {
-        userName: 'Admin',
-        cpf: '00000000000',
-        phone: '0000000000000',
-        email: 'admin@prisma.io',
-        password:  hashPassword,
-        addressStreet:"teste",
-        addressNumber:"1323",
-        addressDistrict:"tegfv",
-        addressCity:"ygsygvsy",
-        addressState:"udhiughg",
-        addressCEP:"78844548",
-        addressComp:"yudgsbbb",
-        addressIbge:"78522135",
-        type: 'ADM',
-        MasterUser: {
-          create: {
-            matricula: '1234567989',
-            position: 'Tenente',
-            createdNewUser: true,
-            createdByUserId: 1,
-          },
+  const hashPassword = await bcrypt.hash("987456321", 6);
+  await prisma.user.create({
+    data: {
+      userName: "Admin",
+      cpf: "00000000000",
+      phone: "0000000000000",
+      email: "admin@prisma.io",
+      password: hashPassword,
+      addressStreet: "teste",
+      addressNumber: "1323",
+      addressDistrict: "tegfv",
+      addressCity: "ygsygvsy",
+      addressState: "udhiughg",
+      addressCEP: "78844548",
+      addressComp: "yudgsbbb",
+      addressIbge: "78522135",
+      type: "ADM",
+      MasterUser: {
+        create: {
+          matricula: "1234567989",
+          position: "Tenente",
+          createdNewUser: true,
+          createdByUserId: 1,
         },
       },
-    })
+    },
+  });
 
   // await prisma.vehicle.create({
   //   data: {
@@ -42,7 +41,9 @@ async function seed() {
   //   },
   // })
 
- 
+  await prisma.$disconnect();
+
+  return;
 }
 
-seed()
+seed();
